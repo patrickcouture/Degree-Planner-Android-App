c5396c264092758455b8c856c4b7400d398add48
patrickcouture <mr.patrickcouture@gmail.com>
2023-03-15 20:25:08 -0600

    First commit

---
+++ app/src/main/java/com/example/degreeplanner_pcouturec196/UI/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.degreeplanner_pcouturec196.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.degreeplanner_pcouturec196.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchTermList (View v) {
+
+        Intent i = new Intent(this, TermList.class);
+        startActivity(i);
+    }
+}
