d0152c6c6b2f365352865b1af20264bc13a0c934
patrickcouture <mr.patrickcouture@gmail.com>
2023-03-15 20:32:31 -0600

    Second Commit

---
+++ app/src/main/java/com/example/degreeplanner_pcouturec196/UI/AssessmentList.java
@@ -0,0 +1,16 @@
+package com.example.degreeplanner_pcouturec196.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.degreeplanner_pcouturec196.R;
+
+public class AssessmentList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessment_list);
+    }
+}
